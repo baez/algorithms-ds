3e0d4d97369b35bb60394c7e66e9aa440e46e3b6
George <george.bekari@gmail.com>
2023-07-14 21:52:37 -0700

    Save work.

--- george2307/george2307.cpp
+++ george2307/george2307.cpp
@@ -1,13 +1,47 @@
 // george2307.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
+#include <string>
+
+int EvaluateCommandStack(const std::string& option)
+{
+    if (option == "create")
+    {
+        // create stack
+    }
+
+    return 0;
+}
+
+void PrintOptionsStack()
+{
+    std::cout << "-- create" << std::endl;
+    std::cout << "-- exit" << std::endl;
+}
 
 int main()
 {
-    // test
-    std::cout << "Hello World!\n";
-    //  a new change that I made 2023-07-14 8:13 PM
+    std::cout << "Welcome to my REPL \n";
 
+    //  main loop
+    while (true)
+    {
+        PrintOptionsStack();
+        std::cout << "Please Enter Option: ";
+
+        std::string input;
+        std::getline(std::cin, input);
+        if (input == "exit")
+        {
+            break;
+        }
+        
+        EvaluateCommandStack(input);
+    }
+
+    std::cout << "Thanks and goodbye!" << std::endl;
 }
+
+
 
 
