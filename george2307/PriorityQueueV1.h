2366c6d8e562b913a2f8f6c8820fb18014e05407
George <george.bekari@gmail.com>
2023-09-08 19:23:23 -0700

    Implement the elementary priority queue.

---
+++ george2307/PriorityQueueV1.h
@@ -0,0 +1,30 @@
+#pragma once
+
+//values:  5, 7, 11, 1
+// index:  0, 1,  2, 3
+// Max PQ
+// Implement the elementary priority queue.
+// 
+class PriorityQueueV1
+{
+public:
+	int Dequeue()
+	{
+
+	}
+
+	void Enqueue(int item)
+	{
+
+	}
+
+	int Peek()
+	{
+
+	}
+
+	bool IsFull()
+	{
+
+	}
+};
